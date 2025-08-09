eb136abb2f9530ff5e578c8078a438145b740323
Arjun-dot-com <arjunvijaykumar06@gmail.com>
2025-08-09 23:00:32 +0530

    Add files via upload

--- PaintOnScreen.py
+++ PaintOnScreen.py
@@ -1,37 +1,38 @@
 import cv2 as cv
 import numpy as np
-from stackimages import stack
 
+paused=False
 cap=cv.VideoCapture(0)
 cap.set(3,640)  # id for width is 3
 cap.set(4,480)  # id for height is 4
 cap.set(10,150) # id for brightness is 10
 
 #make the below nested lists if need more colors
-myColours=[133,204,0,179,255,255] #first all mins then all max
-myColourValues=[0,0,255] #BGR color code for the marker color
+myColours=[[173,119,0,179,255,255],[111,103,0,124,255,255],[91,61,85,103,101,255]] #first all mins then all max
+myColourValues=[[0,0,255],[255,0,0],[0,255,0]] #BGR color code for the marker color
 myPoints=[] # [x,y,Color_idx(if more than one color)]
 
 def findColours(img, myColours, myColourValues):
     imgHSV=cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    newPoint=[]
-    lower=np.array(myColours[:3])
-    upper=np.array(myColours[3:6])
-    mask=cv.inRange(imgHSV, lower, upper)
-    x,y=getCountours(mask)
-    if x!=0 and y!=0:
-        newPoint=[x, y]
-        cv.circle(imgResult, (x, y), 10, myColourValues, cv.FILLED)
-    return newPoint
-
+    newPoints=[]
+    for i,color in enumerate(myColours):
+        lower=np.array(color[0:3])
+        upper=np.array(color[3:6])
+        mask=cv.inRange(imgHSV, lower, upper)
+        x,y=getCountours(mask)
+        if x!=0 and y!=0:
+            newPoints.append([x, y, i])
+            cv.circle(imgResult, (x, y), 5, myColourValues[i], cv.FILLED)
+    return newPoints
 
 def getCountours(img):
     countours,hierarchy=cv.findContours(img, cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
     x,y,w,h=0,0,0,0
+    maxArea=0
     for cnt in countours:
         area=cv.contourArea(cnt)
-        if area>500:
-            #cv.drawContours(imgResult,cnt, -1, (255,0,0),3)
+        if area>500 and area>maxArea:
+            maxArea=area
             peri=cv.arcLength(cnt,True)
             approx=cv.approxPolyDP(cnt,0.02*peri,True)
             x,y,w,h=cv.boundingRect(approx)
@@ -39,26 +40,33 @@ def getCountours(img):
 
 def colorOnCanvas(myPoints, myColourValues):
     for point in myPoints:
-        cv.circle(imgResult, (point[0], point[1]), 8, myColourValues, cv.FILLED)
+        cv.circle(imgResult, (point[0], point[1]), 5, myColourValues[point[2]], cv.FILLED)
 
 while True:
     success,img=cap.read()
-    imgResult=img.copy()
     if not success:
         break
     
-    newPoint=findColours(img, myColours, myColourValues)
-    if len(newPoint)!=0:
-        myPoints.append(newPoint)
-    
+    imgResult=img.copy()
+    key=cv.waitKey(1) & 0xFF
+    if key==ord('s'):
+        paused=not paused
+    elif key==ord('q'):
+        break
+
+    #detect and add new points if NOT paused
+    if not paused:
+        newPoint=findColours(img, myColours, myColourValues)
+        if newPoint:
+            myPoints.extend(newPoint)
+
     if len(myPoints)!=0:
         colorOnCanvas(myPoints, myColourValues)
-    
+            
     imgFinal1=cv.resize(imgResult, (640,480))
     imgFinal=cv.flip(imgFinal1,1)
     cv.imshow("Result",imgFinal)
-    if cv.waitKey(1) & 0xFF==ord('q'):
-        break
 
 cap.release()
 cv.destroyAllWindows()
+    
